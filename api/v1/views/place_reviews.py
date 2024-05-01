5e767d2ac10f83362c1056a5049fd44e16112f35
Etta-Diego <onyinyechiikediego@gmail.com>
2024-05-01 22:11:06 +0000

    added place_feviews.py

--- api/v1/views/place_reviews.py
+++ api/v1/views/place_reviews.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-Flask route for managing reviews associated with places
-"""
+# -*- coding: utf-8 -*-
+"""Flask route for managing reviews associated with places"""
 from flask import Blueprint, jsonify, request, abort
 from api.v1.views import app_views
 from models import storage
@@ -11,50 +10,49 @@ from models.review import Review
 
 @app_views.route('/places/<string:place_id>/reviews', methods=['GET', 'POST'],
                  strict_slashes=False)
-def handle_reviews_per_place(place_id):
-    """Handles requests for reviews associated with a specific place."""
+def places_reviews(place_id):
+    """Create a new view for handling default RESTful API actions related to reviews within a place."""
     place = storage.get('Place', place_id)
     if place is None:
         abort(404)
     if request.method == 'GET':
-        return jsonify([val.to_dict() for val in place.reviews])
+        return jsonify([value.to_dict() for value in place.reviews])
     elif request.method == 'POST':
-        post = request.get_json()
-        if post is None or type(post) != dict:
-            return jsonify({'error': 'Invalid JSON'}), 400
-        elif post.get('user_id') is None:
+        request_json = request.get_json()
+        if request_json is None or type(request_json) != dict:
+            return jsonify({'error': 'Not a JSON'}), 400
+        elif request_json.get('user_id') is None:
             return jsonify({'error': 'Missing user_id'}), 400
-        elif post.get('text') is None:
+        elif request_json.get('text') is None:
             return jsonify({'error': 'Missing text'}), 400
-        elif storage.get('User', post.get('user_id')) is None:
+        elif storage.get('User', request_json.get('user_id')) is None:
             abort(404)
-        new_review = Review(place_id=place_id, **post)
-        new_review.save()
-        return jsonify(new_review.to_dict()), 201
+        new_object = Review(place_id=place_id, **request_json)
+        new_object.save()
+        return jsonify(new_object.to_dict()), 201
 
 
 @app_views.route('/reviews/<string:review_id>',
                  methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
 def get_place_review_id(review_id):
     """Retrieves a review object with a specific ID"""
-    review = storage.get('Review', review_id)
-    if review is None:
+    review_object = storage.get('Review', review_id)
+    if review_object is None:
         abort(404)
     elif request.method == 'GET':
-        return jsonify(review.to_dict())
+        return jsonify(review_object.to_dict())
     elif request.method == 'DELETE':
-        review = storage.get('Review', review_id)
-        storage.delete(review)
+        review_object = storage.get('Review', review_id)
+        storage.delete(review_object)
         storage.save()
         return jsonify({}), 200
     elif request.method == 'PUT':
-        put = request.get_json()
-        if put is None or type(put) != dict:
-            return jsonify({'error': 'Invalid JSON'}), 400
-        for key, value in put.items():
+        insert = request.get_json()
+        if insert is None or type(insert) != dict:
+            return jsonify({'error': 'Not a JSON'}), 400
+        for key, value in insert.items():
             if key not in ['id', 'created_at', 'updated_at',
                            'place_id', 'user_id']:
-                setattr(review, key, value)
+                setattr(review_object, key, value)
                 storage.save()
-        return jsonify(review.to_dict()), 200
-
+        return jsonify(review_object.to_dict()), 200
